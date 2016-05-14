3f67fdf52820f94566787c6e65f91b294d339515
Chris Lewis <chris@evertoon.com>
2016-05-14 15:13:03 -0700

    gets the _ball_ rolling

---
+++ Assets/BallController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour {
+  private Rigidbody _rigidbody;
+  public float power = 5f;
+
+  private float _angle = 0f;
+
+  private void Awake() {
+    _rigidbody = GetComponent<Rigidbody>();
+  }
+
+  private void Update() {
+    if (Input.GetKey(KeyCode.UpArrow)) {
+      _rigidbody.AddTorque(new Vector3(1f, 0f, 0f) * power);
+    }
+
+    if (Input.GetKey(KeyCode.DownArrow)) {
+      _rigidbody.AddTorque(new Vector3(-1f, 0f, 0f) * power);
+    }
+
+    if (Input.GetKey(KeyCode.LeftArrow)) {
+      _rigidbody.AddTorque(new Vector3(0f, 1f, 0f) * power);
+    }
+
+    if (Input.GetKey(KeyCode.RightArrow)) {
+      _rigidbody.AddTorque(new Vector3(0f, -1f, 0f) * power);
+    }
+  }
+}
---
+++ Assets/FollowCam.cs
@@ -0,0 +1,18 @@
+﻿// Copyright 2016 Evertoon.
+//
+//
+
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+  public Transform target;
+  private Vector3 _offset;
+
+  public void Start() {
+    _offset = transform.position - target.position;
+  }
+
+  public void Update() {
+    transform.position = target.position + _offset;
+  }
+}
