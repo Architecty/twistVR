39202ac874b45a7e0583e648366deb63b8684350
Daniel C <danclayson@gmail.com>
2016-05-14 18:37:37 -0700

    Changes to controlling the bar

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -14,55 +14,67 @@ public class BallController : MonoBehaviour
     _rigidbody = GetComponent<Rigidbody>();
   }
 
-    private void OnEnable()
-    {
-        m_VRInput.OnSwipe += HandleSwipe;
-    }
+    //private void OnEnable()
+    //{
+    //    m_VRInput.OnSwipe += HandleSwipe;
+    //}
 
 
-    private void OnDisable()
-    {
-        m_VRInput.OnSwipe -= HandleSwipe;
-    }
+    //private void OnDisable()
+    //{
+    //    m_VRInput.OnSwipe -= HandleSwipe;
+    //}
 
 
     //Handle the swipe events by applying AddTorque to the Ridigbody
-    private void HandleSwipe(VRInput.SwipeDirection swipeDirection)
-    {
-        switch (swipeDirection)
-        {
-            case VRInput.SwipeDirection.NONE:
-                break;
-            case VRInput.SwipeDirection.UP:
-                _rigidbody.AddTorque(new Vector3(0f, 0f, 1f) * power);
-                break;
-            case VRInput.SwipeDirection.DOWN:
-                _rigidbody.AddTorque(new Vector3(0f, 0f, -1f) * power);
-                break;
-            case VRInput.SwipeDirection.LEFT:
-                _rigidbody.AddTorque(new Vector3(-1f, 0f, 0f) * power);
-                break;
-            case VRInput.SwipeDirection.RIGHT:
-                _rigidbody.AddTorque(new Vector3(1f, 0f, 0f) * power);
-                break;
-        }
-    }
+    //private void HandleSwipe(VRInput.SwipeDirection swipeDirection)
+    //{
+    //    switch (swipeDirection)
+    //    {
+    //        case VRInput.SwipeDirection.NONE:
+    //            break;
+    //        case VRInput.SwipeDirection.UP:
+    //            _rigidbody.AddTorque(new Vector3(0f, 0f, 1f) * power);
+    //            break;
+    //        case VRInput.SwipeDirection.DOWN:
+    //            _rigidbody.AddTorque(new Vector3(0f, 0f, -1f) * power);
+    //            break;
+    //        case VRInput.SwipeDirection.LEFT:
+    //            _rigidbody.AddTorque(new Vector3(-1f, 0f, 0f) * power);
+    //            break;
+    //        case VRInput.SwipeDirection.RIGHT:
+    //            _rigidbody.AddTorque(new Vector3(1f, 0f, 0f) * power);
+    //            break;
+    //    }
+    //}
 
     private void Update() {
-            if (Input.GetButton("Forward")) {
-              _rigidbody.AddTorque(new Vector3(1f, 0f, 0f) * power);
-            }
+        if (Input.GetButton("Fire1"))
+        {
+            Vector2 mousePosition = Input.mousePosition;
+            float screenHeight = Screen.height;
+            float screenWidth = Screen.width;
 
-            if (Input.GetButton("Backward")) {
-              _rigidbody.AddTorque(new Vector3(-1f, 0f, 0f) * power);
-            }
+            float newY = (mousePosition.x - (screenWidth / 2f)) / (screenWidth / 2f);
+            float newX = (mousePosition.y - (screenHeight / 2f)) / (screenHeight / 2f);
 
-            if (Input.GetButton("Left")) {
-              _rigidbody.AddTorque(new Vector3(0f, 0f, 1f) * power);
-            }
+            _rigidbody.AddTorque(new Vector3(newY, 0f, -newX) * power);
+        }
 
-            if (Input.GetButton("Right")) {
-              _rigidbody.AddTorque(new Vector3(0f, 0f, -1f) * power);
-            }
+            //if (Input.GetButton("Forward")) {
+            //  _rigidbody.AddTorque(new Vector3(1f, 0f, 0f) * power);
+            //}
+
+            //if (Input.GetButton("Backward")) {
+            //  _rigidbody.AddTorque(new Vector3(-1f, 0f, 0f) * power);
+            //}
+
+            //if (Input.GetButton("Left")) {
+            //  _rigidbody.AddTorque(new Vector3(0f, 0f, 1f) * power);
+            //}
+
+            //if (Input.GetButton("Right")) {
+            //  _rigidbody.AddTorque(new Vector3(0f, 0f, -1f) * power);
+            //}
     }
 }
