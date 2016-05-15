5b511221fad7606af06d747b0161a60750f27a9b
Daniel C <danclayson@gmail.com>
2016-05-14 18:09:46 -0700

    More VR stuff

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
+using VRStandardAssets.Utils;
 
-public class BallController : MonoBehaviour {
-  private Rigidbody _rigidbody;
+public class BallController : MonoBehaviour
+{
+    [SerializeField]
+    private VRInput m_VRInput;
+    private Rigidbody _rigidbody;
   public float power = 5f;
 
   private float _angle = 0f;
@@ -10,21 +14,55 @@ public class BallController : MonoBehaviour {
     _rigidbody = GetComponent<Rigidbody>();
   }
 
-  private void Update() {
-    if (Input.GetKey(KeyCode.UpArrow)) {
-      _rigidbody.AddTorque(new Vector3(1f, 0f, 0f) * power);
+    private void OnEnable()
+    {
+        m_VRInput.OnSwipe += HandleSwipe;
     }
 
-    if (Input.GetKey(KeyCode.DownArrow)) {
-      _rigidbody.AddTorque(new Vector3(-1f, 0f, 0f) * power);
+
+    private void OnDisable()
+    {
+        m_VRInput.OnSwipe -= HandleSwipe;
     }
 
-    if (Input.GetKey(KeyCode.LeftArrow)) {
-      _rigidbody.AddTorque(new Vector3(0f, 0f, 1f) * power);
+
+    //Handle the swipe events by applying AddTorque to the Ridigbody
+    private void HandleSwipe(VRInput.SwipeDirection swipeDirection)
+    {
+        switch (swipeDirection)
+        {
+            case VRInput.SwipeDirection.NONE:
+                break;
+            case VRInput.SwipeDirection.UP:
+                _rigidbody.AddTorque(new Vector3(0f, 0f, 1f) * power);
+                break;
+            case VRInput.SwipeDirection.DOWN:
+                _rigidbody.AddTorque(new Vector3(0f, 0f, -1f) * power);
+                break;
+            case VRInput.SwipeDirection.LEFT:
+                _rigidbody.AddTorque(new Vector3(-1f, 0f, 0f) * power);
+                break;
+            case VRInput.SwipeDirection.RIGHT:
+                _rigidbody.AddTorque(new Vector3(1f, 0f, 0f) * power);
+                break;
+        }
     }
 
-    if (Input.GetKey(KeyCode.RightArrow)) {
-      _rigidbody.AddTorque(new Vector3(0f, 0f, -1f) * power);
+    private void Update() {
+            if (Input.GetButton("Forward")) {
+              _rigidbody.AddTorque(new Vector3(1f, 0f, 0f) * power);
+            }
+
+            if (Input.GetButton("Backward")) {
+              _rigidbody.AddTorque(new Vector3(-1f, 0f, 0f) * power);
+            }
+
+            if (Input.GetButton("Left")) {
+              _rigidbody.AddTorque(new Vector3(0f, 0f, 1f) * power);
+            }
+
+            if (Input.GetButton("Right")) {
+              _rigidbody.AddTorque(new Vector3(0f, 0f, -1f) * power);
+            }
     }
-  }
 }
